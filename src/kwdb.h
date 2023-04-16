525623776bd0c636b21892434d2301d35f9b33d5
Mike Fitzpatrick <fitz@noao.edu>
2023-04-15 20:03:11 -0700

    updated prototypes

--- src/kwdb.h
+++ src/kwdb.h
@@ -3,30 +3,36 @@
  */
 typedef void *pointer;
 
-int kwdb_AddEntry (/* kwdb, keyword, value, type, comment */);
-void kwdb_Close (/* kwdb) */);
-int kwdb_CopyEntry (/* kwdb, o_kwdb, o_ep) */);
-int kwdb_DeleteEntry (/* kwdb, ep) */);
-char *kwdb_GetComment (/* kwdb, keyword) */);
-int kwdb_GetEntry (/* kwdb, ep, keyword, value, type, comment */);
-char *kwdb_GetType (/* kwdb, keyword) */);
-char *kwdb_GetValue (/* kwdb, keyword) */);
-int kwdb_Head (/* kwdb) */);
-int kwdb_Len (/* kwdb) */);
-int kwdb_Lookup (/* kwdb, keyword) */);
-char *kwdb_Name (/* kwdb) */);
-char *kwdb_KWName (/* kwdb, ep) */);
-int kwdb_Next (/* kwdb, ep */);
-pointer kwdb_Open (/* kwdbname */);
-int kwdb_SetComment (/* kwdb, keyword, comment */);
-int kwdb_SetType (/* kwdb, keyword, type */);
-int kwdb_SetValue (/* kwdb, keyword, value */);
-int kwdb_Tail (/* kwdb */);
+pointer kwdb_Open (char *kwdbname);
+void    kwdb_Close (pointer kwdb);
+char   *kwdb_Name (pointer kwdb);
+int     kwdb_Len (pointer kwdb);
+int     kwdb_AddEntry (pointer kwdb, char *keyword, char *value,
+                       char *type, char *comment);
+int     kwdb_Lookup (pointer kwdb, char *keyword, int instance);
+char   *kwdb_GetValue (pointer kwdb, char *keyword);
+int     kwdb_SetValue (pointer kwdb, char *keyword, char *value);
+int     kwdb_SetComment (pointer kwdb, char *keyword, char *comment);
+char   *kwdb_GetComment (pointer kwdb, char *keyword);
+int     kwdb_SetType (pointer kwdb, char *keyword, char *type);
+char   *kwdb_GetType (pointer kwdb, char *keyword);
+int     kwdb_Head (pointer kwdb);
+int     kwdb_Tail (pointer kwdb);
+int     kwdb_Next (pointer kwdb, int ep);
+int     kwdb_DeleteEntry (pointer kwdb, int ep);
+int     kwdb_RenameEntry (pointer kwdb, int ep, char *newname);
+int     kwdb_CopyEntry (pointer kwdb, pointer o_kwdb, int o_ep, char *newname);
+int     kwdb_GetEntry (pointer kwdb, int ep, char **keyword, char **value,
+                       char **type, char **comment);
+char   *kwdb_KWName (pointer kwdb, int ep);
 
-pointer kwdb_OpenFITS (/* filename, maxcards, nblank */);
-int kwdb_UpdateFITS (/* kwdb, filename, update, extend, npad */);
-int kwdb_ReadFITS (/* kwdb, fd, maxcards, nblank */);
-int kwdb_WriteFITS (/* kwdb, fd */);
+pointer kwdb_OpenFITS (char *filename, int maxcards, int *nblank);
+int     kwdb_ReadFITS (pointer kwdb, int fd, int maxcards, int *nblank);
+int     kwdb_UpdateFITS (register KWDB *kwdb, char *filename,
+                         int update, int extend, int npad);
+int     kwdb_WriteFITS (KWDB *kwdb, int fd);
+void    kwdb_SetIO (register KWDB *kwdb,
+                    ssize_t (*readfcn)(), ssize_t (*writefcn)());
 
 /* Compatibility garbage. */
 #ifndef SEEK_SET
